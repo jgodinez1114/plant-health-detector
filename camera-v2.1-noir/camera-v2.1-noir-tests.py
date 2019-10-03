2565cc0b22a66276e7da6ea73a7771d8b7494597
Joel Godinez <jgodinez1114@gmail.com>
2019-10-03 15:08:11 -0500

    Added directive for the boot loader

--- camera-v2.1-noir/camera-v2.1-noir-tests.py
+++ camera-v2.1-noir/camera-v2.1-noir-tests.py
@@ -1,7 +1,11 @@
 # test noir v2.1 camera module
 # 3 Oct 2019
 # Joel Godinez
-# 
+#
+
+# tell boot loader which python environment we are using
+#! /usr/bin/env python 
+
 
 from picamera import PiCamera
 from time import sleep
@@ -9,7 +13,7 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-sleep(5)
+sleep(50)
 camera.stop_preview()
 
 # optional rotation statements
