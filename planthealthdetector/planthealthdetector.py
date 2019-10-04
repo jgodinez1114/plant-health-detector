bc2aec44b9d5d87d17727e1b3ec11eeb76a84822
Joel Godinez <jgodinez1114@gmail.com>
2019-10-03 21:11:18 -0500

    Created driver program

---
+++ planthealthdetector/planthealthdetector.py
@@ -0,0 +1,17 @@
+#import time, datetime, Queue modules
+
+#import DHT, picamera, RPi.GPIO 
+
+#call set up modules
+
+#function to launch analog to digital converter
+
+#function to launch dht11 sensor
+
+#function to launch soil and moisture sensor
+
+#function to launch camera manager
+
+#main function
+
+if __name__=='__main__':#launch if this is the main script running
