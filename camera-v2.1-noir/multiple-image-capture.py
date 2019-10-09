9933f7f7c8981a4cf0f0c7c8ba4edfd789ea7a68
Joel Godinez <jgodinez1114@gmail.com>
2019-10-08 20:11:55 -0500

    Added timestamp information to image capture

---
+++ camera-v2.1-noir/multiple-image-capture.py
@@ -0,0 +1,22 @@
+# 4 Oct 2019
+# Joel Godinez
+
+import time
+import picamera
+import datetime
+
+with picamera.PiCamera() as camera:
+    # capture High Definition image
+    camera.resolution = (1024, 768)
+    #camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+
+    # name the files in the order they were captured
+    for img in range(5):
+        # save the timestamp 
+        date = datetime.datetime.now().stftime(%Y_%m_%d_%H_%M_%S)
+        imageName="phdImage"+ date +".jpg"
+        camera.capture(imageName)
+        time.sleep(5)
+    #camera.stop_preview()
